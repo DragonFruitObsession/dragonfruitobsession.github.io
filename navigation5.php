f8cdbb10e43cd84b53ab183e36a0ea860467bf7b
OrigamiDF <74578233+DragonFruitObsession@users.noreply.github.com>
2021-07-13 12:44:27 +0800

    testing php nav on github

---
+++ navigation5.php
@@ -0,0 +1,24 @@
+<link href="navigation.css" type="text/css" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Sansita+Swashed:wght@300;400&display=swap" rel="stylesheet">
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+
+<!-- navigation bar with five sections -->
+<h2 class="title"><a href="../index.html">Dragon Fruit Obsession</a></h2>
+
+<nav class="navbar">
+  <a href="index.html" class="nav">HOME</a>
+  <a href="analysis/data-analysis.html" class="nav">DATA ANALYSIS</a>
+  <div class="dropdown nav">
+    <button class="dropbtn">HOW TO GROW <i class="fa fa-caret-down"></i></button>
+    <div class="dropdown-content">
+      <a href="how-to-grow/basic-care/basic-care.html">Basic Care</a>
+      <a href="how-to-grow/propagation/propagation.html">Propagation</a>
+      <a href="how-to-grow/trellising/trellising.html">Trellising</a>
+      <a href="how-to-grow/pollination-and-harvest/pollination-and-harvest.html">Pollination and Harvest</a>
+    </div>
+  </div>
+  <a href="origami/origami.html" class="nav">ORIGAMI</a>
+
+  <!-- This one changes depending on the page, need to work out something here-->
+  <a href="#" class="nav" id="active"></a>
+</nav>
