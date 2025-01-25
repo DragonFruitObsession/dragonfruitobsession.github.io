1d0a79b294d0d14c03b3beadd87b456d02f03183
OrigamiTaxonomy <samuel.hesp7@gmail.com>
2025-01-25 14:04:44 +0800

    Way more analysis!

--- analysis/analysis.js
+++ analysis/analysis.js
@@ -5,9 +5,13 @@ function AltImage21() {
 if (mediaQuery.matches) {
 document.getElementById("graphs-2021").style.display = "flex";
 document.getElementById("graphs-2022").style.display = "none";
+document.getElementById("graphs-2023").style.display = "none";
+document.getElementById("graphs-2024").style.display = "none";
 } else {
 document.getElementById("graphs-2021").style.display = "flex";
 document.getElementById("graphs-2022").style.display = "none";
+document.getElementById("graphs-2023").style.display = "none";
+document.getElementById("graphs-2024").style.display = "none";
 }
 }
 
@@ -15,8 +19,40 @@ function AltImage22() {
 if (mediaQuery.matches) {
 document.getElementById("graphs-2022").style.display = "flex";
 document.getElementById("graphs-2021").style.display = "none";
+document.getElementById("graphs-2023").style.display = "none";
+document.getElementById("graphs-2024").style.display = "none";
 } else {
 document.getElementById("graphs-2022").style.display = "flex";
 document.getElementById("graphs-2021").style.display = "none";
+document.getElementById("graphs-2023").style.display = "none";
+document.getElementById("graphs-2024").style.display = "none";
+}
+}
+
+function AltImage23() {
+if (mediaQuery.matches) {
+document.getElementById("graphs-2023").style.display = "flex";
+document.getElementById("graphs-2021").style.display = "none";
+document.getElementById("graphs-2022").style.display = "none";
+document.getElementById("graphs-2024").style.display = "none";
+} else {
+document.getElementById("graphs-2023").style.display = "flex";
+document.getElementById("graphs-2021").style.display = "none";
+document.getElementById("graphs-2022").style.display = "none";
+document.getElementById("graphs-2024").style.display = "none";
+}
+}
+
+function AltImage24() {
+if (mediaQuery.matches) {
+document.getElementById("graphs-2024").style.display = "flex";
+document.getElementById("graphs-2021").style.display = "none";
+document.getElementById("graphs-2022").style.display = "none";
+document.getElementById("graphs-2023").style.display = "none";
+} else {
+document.getElementById("graphs-2024").style.display = "flex";
+document.getElementById("graphs-2021").style.display = "none";
+document.getElementById("graphs-2022").style.display = "none";
+document.getElementById("graphs-2023").style.display = "none";
 }
 }
